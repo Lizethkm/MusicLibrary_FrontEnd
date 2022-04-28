06c3c77bb85abd45f988dcb91d7fee76d59eaae5
Lizeth <mendezlizeth084@gmail.com>
2022-04-28 14:44:13 -0700

    Installed Axios Package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
+
+
 
 function App() {
+
+  const [entries, setEntries] = useState([''])
+
+
   return (
     <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Music Library</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }

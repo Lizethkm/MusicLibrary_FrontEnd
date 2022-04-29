dd15cf482ce381eef1c58d3198dd315ad81cd2d8
Lizeth <mendezlizeth084@gmail.com>
2022-04-28 22:25:26 -0700

    Fixed Naming Error in CreateNewSong Component

--- src/Components/CreateNewSong/CreateNewSong.jsx
+++ src/Components/CreateNewSong/CreateNewSong.jsx
@@ -36,23 +36,23 @@ const CreateNewSong = (props) => {
             <h2>Add Song</h2>
             <div>
                 <label>Title</label>
-                <input type='text' value={title} placeHolder='Title' onChange={(event) => setTitle(event.target.value)} />
+                <input type='text' value={title} placeholder='Title' onChange={(event) => setTitle(event.target.value)} />
             </div>
             <div>
                 <label>Artist</label>
-                <input type='text' value={artist} placeHolder='Artist' onChange={(event) => setArtist(event.target.value)} />
+                <input type='text' value={artist} placeholder='Artist' onChange={(event) => setArtist(event.target.value)} />
             </div>
             <div>
                 <label>Album</label>
-                <input type='text' value={album} placeHolder='Album' onChange={(event) => setAlbum(event.target.value)} />
+                <input type='text' value={album} placeholder='Album' onChange={(event) => setAlbum(event.target.value)} />
             </div>
             <div>
                 <label>Release Date</label>
-                <input type='date' value={release_date} placeHolder='Release Date' onChange={(event) => setRelease_Date(event.target.value)} />
+                <input type='date' value={release_date} placeholder='Release Date' onChange={(event) => setRelease_Date(event.target.value)} />
             </div>
             <div>
                 <label>Genre</label>
-                <input type='text' value={genre} placeHolder='Genre' onChange={(event) => setGenre(event.target.value)} />
+                <input type='text' value={genre} placeholder='Genre' onChange={(event) => setGenre(event.target.value)} />
             </div>
             <button type='submit'>Submit</button>
 
